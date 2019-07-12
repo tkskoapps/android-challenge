8ba8a641cd4f135c6d44500c4cca390321754c62
tkskoapps <niko04@gmail.com>
2019-07-12 01:09:22 -0300

    PostsAdapter and list item layout. Added Glide and ConstrainLayout dependencies.

---
+++ app/src/main/java/com/tkskoapps/redditclient/ui/posts/PostsAdapter.kt
@@ -0,0 +1,72 @@
+package com.tkskoapps.redditclient.ui.posts
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.tkskoapps.redditclient.R
+import com.tkskoapps.redditclient.data.model.PostDataModel
+import com.tkskoapps.redditclient.data.model.PostModel
+import kotlinx.android.synthetic.main.list_item_post.view.*
+
+class PostsAdapter(
+    private var items: MutableList<PostModel> = mutableListOf()
+) :
+    RecyclerView.Adapter<PostsAdapter.PostViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_post, parent, false)
+
+        return PostViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+
+        items[position].let { post ->
+
+            holder.bindCardData(post.postData)
+
+        }
+
+    }
+
+    fun updateList(list: MutableList<PostModel>) {
+
+        val count = items.size
+
+        items = list
+
+        this.notifyItemRangeRemoved(0, count)
+
+        this.notifyItemRangeInserted(0, items.size)
+
+    }
+
+    class PostViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        fun bindCardData(post: PostDataModel) {
+
+            Glide.with(itemView.context).load(post.thumbnail).placeholder(R.drawable.empty_placeholder)
+                .into(itemView.list_item_post_thumbnail)
+
+            itemView.list_item_post_title.text = post.title
+
+            itemView.list_item_post_author.text = post.author
+
+            itemView.list_item_post_time_ago.text = "TODO"
+
+            itemView.list_item_post_comments_count.text = post.commentsCount.toString()
+
+            itemView.list_item_post_read_status.visibility = View.VISIBLE
+
+        }
+
+    }
+
+}
--- app/src/main/java/com/tkskoapps/redditclient/ui/posts/PostsFragment.kt
+++ app/src/main/java/com/tkskoapps/redditclient/ui/posts/PostsFragment.kt
@@ -1,10 +1,15 @@
 package com.tkskoapps.redditclient.ui.posts
 
 import android.os.Bundle
+import android.view.View
 import com.tkskoapps.redditclient.R
 import com.tkskoapps.redditclient.ui.core.BaseFragment
+import kotlinx.android.synthetic.main.fragment_posts.*
+import kotlinx.android.synthetic.main.view_recycler_view.*
 
 class PostsFragment : BaseFragment() {
+
+    private lateinit var postsAdapter: PostsAdapter
 
     companion object {
 
@@ -18,7 +23,27 @@ class PostsFragment : BaseFragment() {
 
         super.onCreate(savedInstanceState)
 
+        postsAdapter = PostsAdapter()
+
         retainInstance = true
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+
+        setEmptyViewVisibility()
+
+    }
+
+    private fun setEmptyViewVisibility() {
+
+        val emptyList = postsAdapter.itemCount == 0
+
+        view_recycle_view_layout_empty.visibility = if (emptyList) View.VISIBLE else View.GONE
+
+        fragment_posts_button_clean.visibility = if (!emptyList) View.VISIBLE else View.GONE
 
     }
 
